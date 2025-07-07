26065d7a7773f0323fdf027d1a7982d08d997d74
Shahab <shahab.siampour@gmail.com>
2025-07-07 16:36:00 +0330

    ff

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('upload/', views.upload_book, name='upload_book'),
     path('book/<int:pk>/', views.book_detail, name='book_detail'),
-
 ]
