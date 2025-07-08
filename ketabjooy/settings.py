f2862178358f95f0027f4c2a1035ecda9634cf5e
Shahab Siampour <33950990+ShahabSiam@users.noreply.github.com>
2025-07-08 20:32:21 +0330

    Update settings.py

--- ketabjooy/settings.py
+++ ketabjooy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mbui)7rzzws!726$)ibg(hh-)blup*q9&)e(asmw#(bx$ff*re'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['185.228.236.49','ketabjooy.ir']
 
